f01c120e5031d1186e902151d32f753c80556311
2206695397 <141420138+2206695397@users.noreply.github.com>
2023-08-04 20:58:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',oDRNp68r-qqt_cya-Zqh2ZnZ7T94
+      id: '',wx0bfae8e45914209d
     }
   ],
 
